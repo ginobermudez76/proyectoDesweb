6a8726dc3ac2e4eabe93df3c5dc93bf1565d1260
SaidJuniorProgrammer <saidpintotamayo3731@upse.edu.ec>
2026-06-23 21:39:35 -0500

    fix: refactor de sesiones a redis e incidencias a mongodb

--- backend/app/Models/Incidencia.php
+++ backend/app/Models/Incidencia.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+
+use MongoDB\Laravel\Eloquent\Model; 
 
 class Incidencia extends Model
 {
+    
+    protected $connection = 'mongodb';
+    protected $collection = 'incidencias';
+
     protected $fillable = ['titulo', 'descripcion', 'estado', 'prioridad'];
 }
--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,20 +3,16 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\Sanctum; 
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        
+        Sanctum::ignoreMigrations();
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
